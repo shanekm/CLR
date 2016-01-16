8dbbba36c8021fcfd7cc9f3c057bcf9fd227e7c3
Shane Kmiecik <shane.kmiecik@hotmail.com>
2016-01-16 16:22:50 -0600

    update

--- Fundementals/App/App/Program.cs
+++ Fundementals/App/App/Program.cs
@@ -18,10 +18,36 @@ namespace App
 
             //Console.ReadKey();
 
-
+            B b1 = new B();         // OK
+            D d1 = new D();         // OK
+            B b2 = new D();         // OK - can not call D's methods
+           // B b3 = new Object();    // CTE - base to derived
+            B b4 = d1;              // OK - derived to base - implicit
+           // D d3 = b2;              // CTE - base to derived
+            D d4 = (D)d1;           // OK - D to D,
+            D d5 = (D)b2;           // OK - b2 points to new D(), explicit
+            D d6 = (D)b1;           // RTE - b1 points to new B(), run time error
+            B b6 = (B)b2;           // OK - b2 points to new D()
         }
     }
 
-    public class B { }
-    public class D : B { }
+    public class B
+    {
+        public B()
+        {
+            First = "first";
+        }
+
+        public string First { get; set; }
+    }
+
+    public class D : B
+    {
+        public D()
+        {
+            Second = "second";
+        }
+
+        public string Second { get; set; }
+    }
 }
